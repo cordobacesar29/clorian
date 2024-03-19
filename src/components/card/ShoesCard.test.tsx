0ce1a1c47cb395eeeb82eb9656b0e2c63d76cd9b
Cesar Cordoba <cordobacesar29@gmail.com>
2024-03-19 16:09:39 -0300

    vitest

--- src/components/card/ShoesCard.test.tsx
+++ src/components/card/ShoesCard.test.tsx
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, test, vi, } from 'vitest';
+import { beforeEach, describe, expect, test } from 'vitest';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { ShoesCard } from '.';
 
